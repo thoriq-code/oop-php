8832055854ace485f2a79bffabad917d9d73f602
riiq_07 <54975037+thoriq-code@users.noreply.github.com>
2026-05-07 05:48:27 +0700

    Create Product class with properties

---
+++ product.php
@@ -0,0 +1,13 @@
+<?php
+
+// Book
+class Product {
+  public $title = "Title", 
+         $writer = "Writer", 
+         $publisher = "Publisher", 
+         $price = "Price";  
+}
+
+$product1 = new Product();
+
+?>
