4fcb6a177d4025c133f93caab61ea728720fbd07
riiq_07 <54975037+thoriq-code@users.noreply.github.com>
2026-05-07 06:10:28 +0700

    Update product.php

--- product.php
+++ product.php
@@ -9,5 +9,8 @@ class Product {
 }
 
 $product1 = new Product();
-
+$product1->title = "Harry Potter & Philosopher's Stone";
+$product1->writer = "J.K. Rowling";
+$product1->publisher = "Bloomsbury";
+$product1->price = "$30";
 ?>
